85c4ee7e7758b89606883dcf9f9997827b27774f
natmrcl <91374949+natmrcl@users.noreply.github.com>
2022-04-10 16:47:18 +0700

    Create soal4.R

---
+++ soal4.R
@@ -0,0 +1,17 @@
+#4. Diketahui nilai x = 2 dan v = 10. Tentukan:
+
+x<-2
+v<-10
+
+#a. Fungsi Probabilitas dari Distribusi Chi-Square.
+dchisq(x,v,ncp=0)
+
+#b. Histogram dari Distribusi Chi-Square dengan 100 data random.
+hist(rchisq(100, v), main='Distribusi Chi-Square dengan 100 data random')
+
+#c. Nilai Rataan (µ) dan Varian (s²) dari DistribusiChi-Square.
+rataan=v
+varian=2*v
+
+paste("Nilai Rataan: ", rataan)
+paste("Nilai Varian: ", varian)
